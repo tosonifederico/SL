3f59fe1fcef53fd4548c2e15575e860f205e7c3e
Federico Tosoni <federicotosoni22@gmail.com>
2025-08-06 01:00:32 +0200

    include/internals.h docs

--- include/internals.h
+++ include/internals.h
@@ -19,17 +19,24 @@
 #define MIN(a,b) ((a) < (b) ? a : b)
 
 
+// macros to lock and unlock any mutex
 #define LOCK(m) pthread_mutex_lock(&m)
 #define UNLOCK(m) pthread_mutex_unlock(&m)
 
 
+// prints an error to stderr when a memory allocation fails 
 void throw_memory_allocation_error(const char *file, int line, const char *function);
+
+
+// Wappers to memory allocation function to show in case of failure
+// file, line, and function in which the problem occurred
 void *MallocWrapper(size_t size, const char *file, int line, const char *function);
 void *CallocWrapper(size_t num, size_t size, const char *file, int line, const char *function);
 void *ReallocWrapper(void *ptr, size_t size, const char *file, int line, const char *function);
 void FreeWrapper(void *ptr, const char *file, int line, const char *function);
 
 
+// macros to link memory function to their respective wrappers
 #define MEM_DEBUG
 
 #ifdef MEM_DEBUG
@@ -52,12 +59,18 @@ void FreeWrapper(void *ptr, const char *file, int line, const char *function);
 extern char* strdup(const char*);
 
 
+/*
+    given a void* returns a copy of the ptr allocated in the heap.
+    It doesn't perfom deep copies
+*/
 void* copy_from_void_ptr(const void *src, size_t type_size);
 
 
+// returns true if two void* points to two equal chunks of memory
 bool compare_void_ptr(const void *ptr1, const void *ptr2, size_t type_size1, size_t type_size2);
 
 
+// given a void*, read the first (size) bytes and returns the hex representation in a null-terminated string
 char* hex_string_from_pointer(void* data, size_t size);
 
 
